034a5c6f60913c70091eed8f3ebd22d86412085c
shivam kumar bhagat <54414190+bhagat-shivam@users.noreply.github.com>
2023-08-02 10:50:28 +0530

    3rd commit.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -4,10 +4,10 @@ var icon = document.getElementById("icon");
 icon.onclick = function(){
    var isDarkTheme =  document.body.classList.toggle("dark-theme");
     if(isDarkTheme){
-        icon.src = "portfolio_site_img/moon.png";
+        icon.src = "portfolio_site_img/sun.png";
     } 
     
     else {
-        icon.src = "portfolio_site_img/sun.png";
+        icon.src = "portfolio_site_img/moon.png";
     }
 }
